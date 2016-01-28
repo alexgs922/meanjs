df13a926d5aefdf351a75f6112d5dd83b8a504cc
Alberto Basalo <albertobasalo@hotmail.com>
2016-01-28 18:58:43 +0100

    Sesión 1

--- 00-angularjs/0-hola-angularjs/README.md
+++ 00-angularjs/0-hola-angularjs/README.md
@@ -9,5 +9,11 @@ HolaAngularJS
 Hola Mundo con AngularJS
 ________________________
 
-AngularJS es una técnología que nos facilita enormemente la ccreación de interfaces web.
+AngularJS es una técnología que nos facilita enormemente la creación de interfaces web.
 Puedes comprobar como este humilde *Hola Mundo* se torna potente sólo con añadir un par de *directivas* de AngularJS.
+
+
+Y también sabe sumar ;-)
+
+
+[AngularJS 1.5 CDN](https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0-rc.1/angular.min.js)
--- 03-control-en-javascript/cajaCtrl.js
+++ 03-control-en-javascript/cajaCtrl.js
@@ -4,60 +4,62 @@
 // Pero al mismo tiempo garantiza que se ejecutará automáticamente
 (function () {
 
-        // la función controladora se define con su propio nombre
-        function cajaCtrl() {
-            // El viewmodel es la propia función
-            // Guardamos una referencia a 'this' en una variable que podamos usar dentro de otras funciones
-            var vm = this;
-            // Declaración y asignación de valores iniciales
-            vm.titulo = "Controla tu Cash Flow";
-            vm.total = {
-                ingresos: 0,
-                gastos: 0
-            };
-            vm.maestros = {
-                categoriasIngresos: ['Nómina', 'Ventas', 'Intereses Depósitos'],
-                categoriasGastos: ['Hipotéca', 'Compras', 'Impuestos']
-            };   
-            vm.nuevoMovimiento = {
-                esIngreso: 1,
-                esGasto: 0,
-                importe: 0,
-                fecha: new Date()
-            };
-            // Array en el que guardar los movimientos
-            vm.movimientos = [];
+		// Accedemos al módulo por su nombre a través de la única variable global 'angular'
+		// En dicho módulo creamos un controlador, cuyo código es la función previamente definida
+		angular.module('cashFlow').controller('CajaCtrl', implementacionCajaCtrl);
 
-            // El scope no son sólo datos, también funciones
-            vm.guardarMovimiento = function () {
-                if (vm.nuevoMovimiento.esIngreso) {
-                    vm.total.ingresos += vm.nuevoMovimiento.importe;
-                } else {
-                    vm.total.gastos += vm.nuevoMovimiento.importe;
-                }
-                // creamos una copia de los valores del actual formulario
-                var auxCopyMov = angular.copy(vm.nuevoMovimiento);
-                // USamos funciones de transformación
-                auxCopyMov.tipo = vm.tipo(auxCopyMov);
-                // Se almacena la copia del nuevo movimiento, y se reutiliza la variable para crear otros
-                vm.movimientos.push(auxCopyMov);
-                // El formulario se limpia y la variable se reutiliza
-                vm.nuevoMovimiento.importe = 0;
-            }
+		// la función controladora se define con su propio nombre
+		function implementacionCajaCtrl() {
+			// El viewmodel es la propia función
+			// Guardamos una referencia a 'this' en una variable que podamos usar dentro de otras funciones.
+			var vm = this;
+			// Declaración y asignación de valores iniciales
+			vm.titulo = "Controla tu Cash Flow";
+			vm.total = {
+				ingresos: 0,
+				gastos: 0
+			};
+			vm.maestros = {
+				categoriasIngresos: ['Nómina', 'Ventas', 'Intereses Depósitos'],
+				categoriasGastos: ['Hipotéca', 'Compras', 'Impuestos']
+			};
+			vm.nuevoMovimiento = {
+				esIngreso: 1,
+				esGasto: 0,
+				importe: 0,
+				fecha: new Date()
+			};
+			// Array en el que guardar los movimientos
+			vm.movimientos = [];
 
-            // las funiones puede llamarse desde la vista o desde el código
-            vm.balance = function () {
-                return vm.total.ingresos - vm.total.gastos
-            }
-            vm.tipo = function (movimiento) {
-                return movimiento.esIngreso && 'Ingreso' || 'Gasto'
-            }
-        }
+			// El scope no son sólo datos, también funciones
+			vm.guardarMovimiento = function () {
+				if (vm.nuevoMovimiento.esIngreso) {
+					vm.total.ingresos += vm.nuevoMovimiento.importe;
+				} else {
+					vm.total.gastos += vm.nuevoMovimiento.importe;
+				}
+				// creamos una copia de los valores del actual formulario
+				var auxCopyMov = angular.copy(vm.nuevoMovimiento);
+				// Usamos funciones de transformación
+				auxCopyMov.tipo = vm.tipo(auxCopyMov);
+				// Se almacena la copia del nuevo movimiento, y se reutiliza la variable para crear otros
+				vm.movimientos.push(auxCopyMov);
+				// El formulario se limpia y la variable se reutiliza
+				vm.nuevoMovimiento.importe = 0;
+			}
 
-        // Accedemos al módulo por su nombre a través de la única variable global 'angular'
-        // En dicho módulo creamos un controlador, cuyo código es la función previamente definida
-        angular.module('cashFlow').controller('CajaCtrl', cajaCtrl);
+			// las funiones puede llamarse desde la vista o desde el código
+			vm.balance = function () {
+				return vm.total.ingresos - vm.total.gastos
+			}
+			vm.tipo = function (movimiento) {
+				return movimiento.esIngreso && 'Ingreso' || 'Gasto'
+			}
+		}
 
-    }
 
-    ()); // Sintáxis JS para invocación inmediata
+
+	}
+
+	()); // Sintáxis JS para invocación inmediata
